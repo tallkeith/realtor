cc451db712b5d1cb2d7c3eea8a7538bae42989ff
tallkeith <tallestkeith@gmail.com>
2014-09-06 16:39:19 -0400

    added validators to prospect input fields, added in designer's html and css

--- app/assets/javascripts/prospects.js.coffee
+++ app/assets/javascripts/prospects.js.coffee
@@ -5,4 +5,5 @@
 
 
 jQuery ->
-  $("#prospects").dataTable();
+  $("#prospects").dataTable()
+  pagingType: 'full'
--- app/models/prospect.rb
+++ app/models/prospect.rb
@@ -3,6 +3,10 @@ class Prospect < ActiveRecord::Base
 	geocoded_by :address
 	after_validation :geocode
 
+	validates_format_of :email, :with => /@/, :message => "Please enter a valid email address"
+	validates_presence_of :name
+	validates_length_of :phone, :minimum => 10
+
   def address
     "#{street} #{citystatezip}"
   end
