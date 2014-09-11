5d747c744e9159102c90a749770c199987314135
tallkeith <tallestkeith@gmail.com>
2014-09-11 12:06:58 -0400

    removed looping button from results page

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
 
   post 'admin/prospects/new'
 
+  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
